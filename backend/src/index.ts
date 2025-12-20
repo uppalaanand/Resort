79c67c0388531728a0ac0f3515e44f74ebeacb02
Uppala Anand <uppalaanand410@gmail.com>
2025-12-20 16:11:55 +0530

    Added room request feature

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -44,6 +44,8 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is listening on port ${PORT}`);
+// });
+
+export default app;
